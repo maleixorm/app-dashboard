ae767aa4df404e57606ead30fcef64efb96d2723
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 10:45:02 -0300

    Criando o php para conectar ao banco

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php
+
+// classe dashboard
+class Dashboard {
+    public $data_inicio;
+    public $data_fim;
+    public $numeroVendas;
+    public $totalVendas;
+    public $clientesAtivos;
+}
