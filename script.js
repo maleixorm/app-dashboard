c5f3d5d39bb2aa364ba4d851c5a064f4ba1049e8
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 20:46:15 -0300

    Função que recebe os dados do backend e retorna a informação no formato json para o frontend

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ $(document).ready(() => {
             $('#pagina').html(data);
         });
     });
+    $('#competencia').on('change', e => {
+        let competencia = $(e.target).val();
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => { 
+                $('#numeroVendas').html(dados.numeroVendas);
+                $('#totalVendas').html(dados.totalVendas);
+            },
+            error: erro => { console.log(erro); }
+        });
+    })
 });
