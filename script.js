ee863d09c14d74268a2160effdf3455e9c344b94
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 10:44:37 -0300

    Carregando as páginas de documentação e suporte diretamente com o jQuery

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(() => {
-	
-})
+	$('#documentacao').on('click', () => {
+        $.post('documentacao.html', data => {
+            $('#pagina').html(data);
+        });
+    });
+	$('#suporte').on('click', () => {
+        $.post('suporte.html', data => {
+            $('#pagina').html(data);
+        });
+    });
+});
