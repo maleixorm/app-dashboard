59130907bb9ec25a391ab6a610db5d29cb7da1c7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 16:10:35 -0300

    Criando os metodos mágicos get e set e as classes Conexao e BD

--- app.php
+++ app.php
@@ -7,4 +7,78 @@ class Dashboard {
     public $numeroVendas;
     public $totalVendas;
     public $clientesAtivos;
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+        return $this;
+    }
 }
+
+// classe para conectar com o banco de dados
+class Conexao {
+    private $host = 'localhost';
+    private $dbname = 'dashboard';
+    private $user = 'php';
+    private $pass = 123456;
+
+    public function conectar()
+    {
+        try {
+            $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+            $conexao->exec('set charset utf8');
+            return $conexao;
+        } catch (PDOException $e) {
+            echo '<p>'.$e->getMessage().'</p>';
+        }
+    }
+}
+
+// classe model
+
+class Bd {
+    private $conexao;
+    private $dashboard;
+
+    public function __construct(Conexao $conexao, Dashboard $dashboard)
+    {
+        $this->conexao = $conexao->conectar();
+        $this->dashboard = $dashboard;
+    }
+
+    public function getNumeroVendas()
+    {
+        $query ="SELECT count(*) AS numero_vendas FROM tb_vendas WHERE data_venda BETWEEN :data_inicio AND :data_fim";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+    }
+    
+    public function getTotalVendas()
+    {
+        $query ="SELECT sum(total) AS total_vendas FROM tb_vendas WHERE data_venda BETWEEN :data_inicio AND :data_fim";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+        $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+    }
+}
+
+$conexao = new Conexao();
+$dashboard = new Dashboard();
+$dashboard->__set('data_inicio', '2018-08-01');
+$dashboard->__set('data_fim', '2018-08-30');
+
+$bd = new Bd($conexao, $dashboard);
+
+$dashboard->__set('numeroVendas', $bd->getNumeroVendas());
+$dashboard->__set('totalVendas', $bd->getTotalVendas());
+print_r($dashboard);
